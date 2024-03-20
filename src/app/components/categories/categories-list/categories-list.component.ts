6b0a313f186daa03d3e3e332dd4206bc8d784b86
berkcankucukoglu <berkcankucukoglu@gmail.com>
2024-03-20 21:02:51 +0300

    0.0.1_simple navbar and categories routerModule implemented.

---
+++ src/app/components/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-categories-list',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  templateUrl: './categories-list.component.html',
+  styleUrl: './categories-list.component.css'
+})
+export class CategoriesListComponent {
+
+}
